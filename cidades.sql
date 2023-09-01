cbdadb6a37fe1e9303b23219be11b8565923545a
IagoAbreu01 <143406767+IagoAbreu01@users.noreply.github.com>
2023-09-01 11:14:39 -0300

    Add files via upload
    Criação do banco dados cidades

---
+++ cidades.sql
@@ -0,0 +1,20 @@
+zzzCREATE TABLE cidades(
+    id INTEGER,
+    nome TEXT,
+    uf 	TEXT 
+); 
+
+INSERT INTO cidades (id, nome, uf) VALUES (1,"Gramado", "RS");
+INSERT INTO cidades (id, nome, uf) VALUES (2,"Porto Alegre","RS"); 
+INSERT INTO cidades (id, nome, uf) VALUES (3,"Não me Toque","RS");
+INSERT INTO cidades (id, nome, uf) VALUES (4,"Passo Fundo","RS");
+INSERT INTO cidades (id, nome, uf) VALUES (5,"Carazinho","RS");
+
+SELECT * FROM cidades
+
+UPDATE cidades 
+SET nome = "Gramado"
+WHERE id = 4
+
+DELETE FROM cidade
+WHERE id = 5 
